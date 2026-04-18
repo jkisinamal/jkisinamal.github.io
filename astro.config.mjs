39bee799ca8a8f8666b22ebe61e4b168910b6404
Jero Halili <127023985+JeroH20@users.noreply.github.com>
2026-04-18 17:47:24 +0800

    Undo simplify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
+import { defineConfig } from 'astro/config';
+import sanity from '@sanity/astro';
+import react from '@astrojs/react';
+
 export default defineConfig({
   output: 'static',
   integrations: [
     sanity({
       projectId: process.env.PUBLIC_SANITY_PROJECT_ID,
       dataset: 'production',
+      studioBasePath: process.env.NODE_ENV === 'development' ? '/admin' : undefined,
       useCdn: false,
     }),
     react(),
