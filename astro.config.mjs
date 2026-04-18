b55398e5a31013c975b626337a88957c9b35f284
Jero Halili <127023985+JeroH20@users.noreply.github.com>
2026-04-18 17:12:07 +0800

    Fix add prerender flag

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
-// astro.config.mjs
 import { defineConfig } from 'astro/config';
 import sanity from '@sanity/astro';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  output: 'static', // <--- ADD THIS LINE
+  output: 'static',
   integrations: [
     sanity({
       projectId: process.env.PUBLIC_SANITY_PROJECT_ID,
       dataset: 'production',
-      studioBasePath: '/admin', // This tells Astro /admin is a special case
+      studioBasePath: '/admin',
+      prerender: true, 
       useCdn: false,
     }),
     react(),
